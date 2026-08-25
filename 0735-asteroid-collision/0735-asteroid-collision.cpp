0e2c1f547716898996093be8821e2ee8bb43e417
Aditya_Ahlawat <adityaahlawat544@gmail.com>
2026-08-25 10:24:40 +0530

    Time: 0 ms (100%), Space: 21.6 MB (79.13%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& arr) {
+        vector<int> ans;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] > 0) {
+                ans.push_back(arr[i]);
+            } else {
+                while (!ans.empty() && ans.back() > 0 &&
+                       ans.back() < abs(arr[i])) {
+                    ans.pop_back();
+                }
+                if (!ans.empty() && ans.back() > 0 &&
+                    ans.back() == abs(arr[i])) {
+                    ans.pop_back();
+                } else if (ans.empty() || ans.back() < 0) {
+                    ans.push_back(arr[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
