8c0a41048644ac4b1303db135ce011dc04978969
Aditya_Ahlawat <adityaahlawat544@gmail.com>
2026-09-12 00:18:46 +0530

    Time: 0 ms (100%), Space: 16.5 MB (97.49%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int,int>> q;
+        int fresh = 0;
+
+        // Put all rotten oranges in queue
+        // and count fresh oranges
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(grid[i][j] == 2) {
+                    q.push({i, j});
+                }
+                else if(grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int time = 0;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while(!q.empty() && fresh > 0) {
+
+            int size = q.size();
+
+            // One complete level = one minute
+            for(int i = 0; i < size; i++) {
+
+                auto [r, c] = q.front();
+                q.pop();
+
+                for(int d = 0; d < 4; d++) {
+
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+
+                    if(nr >= 0 && nr < n &&
+                       nc >= 0 && nc < m &&
+                       grid[nr][nc] == 1) {
+
+                        grid[nr][nc] = 2;
+                        fresh--;
+
+                        q.push({nr, nc});
+                    }
+                }
+            }
+
+            time++;
+        }
+
+        return (fresh == 0) ? time : -1;
+    }
+};
