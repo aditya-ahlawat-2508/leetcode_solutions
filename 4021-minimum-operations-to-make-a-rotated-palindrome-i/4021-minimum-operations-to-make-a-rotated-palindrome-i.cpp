5cec6e7538b00ca8142651a3a91168e6555bb1e6
Aditya_Ahlawat <adityaahlawat544@gmail.com>
2026-09-17 16:33:51 +0530

    Time: 219 ms (55.42%), Space: 10.7 MB (79.01%) - LeetHub

--- 4021-minimum-operations-to-make-a-rotated-palindrome-i/4021-minimum-operations-to-make-a-rotated-palindrome-i.cpp
+++ 4021-minimum-operations-to-make-a-rotated-palindrome-i/4021-minimum-operations-to-make-a-rotated-palindrome-i.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int minOperations(string s) {
-    int n = s.size();
-    int minCost = INT_MAX;
+        int n = s.size();
+        int minCost = INT_MAX;
 
-    for (int i = 0; i < n; i++) {
-        // s ab current rotation hai
-        int l = 0, r = n - 1;
-        int cost = 0;
-        while (l < r) {
-            int diff = abs((s[r] - 'a') - (s[l] - 'a'));
-            cost += min(diff, 26 - diff);
-            l++; r--;
-        }
-        minCost = min(minCost, i + cost);
+        for (int i = 0; i < n; i++) {
+            // s ab current rotation hai
+            int l = 0, r = n - 1;
+            int cost = 0;
+            while (l < r) {
+                int diff = abs((s[r] - 'a') - (s[l] - 'a'));
+                cost += min(diff, 26 - diff);
+                l++;
+                r--;
+            }
+            minCost = min(minCost, i + cost);
 
-        // poori string ko ek jagah left rotate karo (agle iteration ke liye)
-        char first = s[0];
-        for (int k = 0; k < n - 1; k++) {
-            s[k] = s[k + 1];
+            // poori string ko ek jagah left rotate karo (agle iteration ke
+            // liye)
+            char first = s[0];
+            for (int k = 0; k < n - 1; k++) {
+                s[k] = s[k + 1];
+            }
+            s[n - 1] = first;
         }
-        s[n - 1] = first;
+        return minCost;
     }
-    return minCost;
-}
     // lowercase english letters
     // perform following ops any no of times including 0 in any order
 };
